cd99fb7e1aa1048b0a14e20f99a9e43056b4379d
Jason Weinreb <jason@Jasons-MacBook-Pro.local>
2019-09-19 11:20:52 -0400

    big commit

---
+++ get_attributes_categories.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+import requests
+
+RAVELRY_USERNAME='4CEB364E4FD3892C1EC0'
+RAVELRY_PASSWORD='61LaiyoHLxlHSCrL69cRQNe4EPtxvaBOEvggWYRJ'
+auth = requests.auth.HTTPBasicAuth(RAVELRY_USERNAME, RAVELRY_PASSWORD)
+
+## Pattern attributes - tags, to add as columns in pattern database
+patt_attrs = requests.get('https://api.ravelry.com/pattern_attributes/groups.json', auth=auth)
+patt_attrs = [grp for grp in patt_attrs.json()['attribute_groups']]
+records = []
+for cat in patt_attrs:
+    for a in cat.get('pattern_attributes'):
+        records.append(a)
+    children = cat['children']
+    if children:
+        for child in children:
+            for a2 in child.get('pattern_attributes'):
+                records.append(a2)
+            childdren = child['children']
+            if childdren:
+                for childd in childdren:
+                   for a3 in childd.get('pattern_attributes'):
+                       records.append(a3)
+      	
+attributes = pd.DataFrame.from_records(records)[['id', 'name', 'description']]
+
+if __name__ == '__main__':
+    print(attributes.head())
+
+
+## Pattern Categories
+patt_cats = requests.get('https://api.ravelry.com/pattern_categories/list.json', auth=auth)
+patt_cats = patt_cats.json()['pattern_categories']['children']
+accessories_and_clothing = [patt_cats[0]] + [patt_cats[1]]
+
+records = []
+for cat in accessories_and_clothing:
+    record = {k: v for k, v in cat.items() if k in ['id', 'long_name']}
+    records.append(record)
+    children = cat['children']
+    if children:
+        for child in children:
+            record =  {k: v for k, v in child.items() if k in ['id', 'long_name']}
+            records.append(record)
+            childdren = child['children']
+            if childdren:
+                for childd in childdren:
+                    record =  {k: v for k, v in childd.items() if k in ['id', 'long_name']}
+                    records.append(record)        	
+categories = pd.DataFrame.from_records(records)
+
+if __name__ == '__main__':
+    print(categories.head())
+
